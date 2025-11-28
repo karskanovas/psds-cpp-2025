414fcf19f5eec3994846f18428d92f768ca577d4
karskanovas <yuktyui47@gmail.com>
2025-11-28 19:56:15 +0500

    Add addition task and update other files

---
+++ build/_deps/googletest-build/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/sveta22/psds-cpp-2025/build/_deps/googletest-src/googletest
+# Build directory: /home/sveta22/psds-cpp-2025/build/_deps/googletest-build/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
